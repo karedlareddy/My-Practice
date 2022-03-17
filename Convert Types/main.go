6f5a8f0b170fcea85361bd820202f4addaf2e4d1
Karedlareddy <bhargavreddy1164@gmail.com>
2022-03-17 15:22:37 +0530

    commit

--- Convert Types/main.go
+++ Convert Types/main.go
@@ -11,4 +11,29 @@ func main() {
 	fmt.Println(reflect.TypeOf(strVar))
 	intVar, err := strconv.Atoi(strVar)
 	fmt.Println(intVar, err, reflect.TypeOf(intVar))
+	two()
+	three()
+}
+
+func two() {
+	strVar := "100"
+
+	intVar, err := strconv.ParseInt(strVar, 0, 8)
+	fmt.Println(intVar, err, reflect.TypeOf(intVar))
+
+	intVar, err = strconv.ParseInt(strVar, 0, 16)
+	fmt.Println(intVar, err, reflect.TypeOf(intVar))
+
+	intVar, err = strconv.ParseInt(strVar, 0, 32)
+	fmt.Println(intVar, err, reflect.TypeOf(intVar))
+
+	intVar, err = strconv.ParseInt(strVar, 0, 64)
+	fmt.Println(intVar, err, reflect.TypeOf(intVar))
+}
+
+func three() {
+	strVar := "100"
+	intValue := 0
+	_, err := fmt.Sscan(strVar, &intValue)
+	fmt.Println(intValue, err, reflect.TypeOf(intValue))
 }
