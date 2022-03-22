a3f01c71d7c0872cac3c84382e9c567e7743fc30
Karedlareddy <bhargavreddy1164@gmail.com>
2022-03-22 15:49:18 +0530

    commit

--- operators/main.go
+++ operators/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var x, y = 35, 7
@@ -16,4 +18,75 @@ func main() {
 
 	y--
 	fmt.Printf("y-- = %d\n", y)
+	one()
+	two()
+	three()
+	four()
+}
+
+func one() {
+	var x, y = 15, 25
+	x = y
+	fmt.Println(x)
+
+	x = 15
+	x += y
+	fmt.Println(x)
+
+	x = 50
+	x -= y
+	fmt.Println(x)
+
+	x = 2
+	x *= y
+	fmt.Println(x)
+
+	x = 100
+	x /= y
+	fmt.Println(x)
+
+	x = 40
+	x %= y
+	fmt.Println(x)
+}
+
+func two() {
+	var x, y = 15, 25
+
+	fmt.Println(x == y)
+	fmt.Println(x != y)
+	fmt.Println(x < y)
+	fmt.Println(x <= y)
+	fmt.Println(x > y)
+	fmt.Println(x >= y)
+}
+
+func three() {
+
+	var x, y, z = 10, 20, 30
+	fmt.Println(x < y && x > z)
+	fmt.Println(x < y || x > z)
+	fmt.Println(!(x == y && x > z))
+}
+
+func four() {
+
+	var x int = 9
+	var y int = 65
+	var z int
+
+	z = x & y
+	fmt.Println(z)
+
+	z = x | y
+	fmt.Println(z)
+
+	z = x ^ y
+	fmt.Println(z)
+
+	z = x << 1
+	fmt.Println(z)
+
+	z = x >> 1
+	fmt.Println(z)
 }
